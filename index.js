fa4dace938610d7b8767e0457e32d132d2944e3f
Ehapa-B <111089992+Ehapa-B@users.noreply.github.com>
2022-12-02 15:03:54 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute(("src"), randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "🏆Draw!";
+}
